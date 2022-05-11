9d3093f9ed0fe5e376fd98304d5e4b6149e00ce9
Kamalesh P <66113370+kamalesh-p@users.noreply.github.com>
2022-05-11 11:12:49 +0530

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -21,6 +21,18 @@ public:
         length = 0; 
         wait = 0;
     }
+    void myname(SOCKET ClientSocketmyname, char name[15]) {
+        Node* thisNode = head;
+        while (thisNode != NULL) {
+            if (ClientSocketmyname == thisNode->ClientSocket) {
+                char message[50] = "\0";
+                sprintf_s(message, "func(%s), list(%s)\0", name, thisNode->name);
+                if (!sendd(ClientSocketmyname, message, thisNode->name))return;
+                break;
+            }
+            thisNode = thisNode->next;
+        }
+    }
     void remove(SOCKET ClientSocket) {
         while (this->wait) {
             cout << "Remove waiting...." << endl;
@@ -38,15 +50,22 @@ public:
                 current = current->next; // go to next value
             }
         }
-        if (current == NULL) { // if we reached end of list or the list is empty
+        if (head != NULL && head->next == NULL && head->ClientSocket == ClientSocket) {
+            cout << "Deleting(" << head->name << ")" << endl;
+            head = NULL;
+        }
+        else if (current == NULL) { // if we reached end of list or the list is empty
             cout << "Can't remove value: no match found.\n";
-            //Sleep(100);
-            //this->wait = 0;
-            //remove(ClientSocket);
+            //if (count < 5) {
+                //Sleep(100);
+                //this->wait = 0;
+                //remove(ClientSocket, count++);
+            //}
         }
         else {
-            cout << "Deleting\n";
+            cout << "Deleting(";
             prev->next = current->next; // unlink the node you remove
+            cout << current->name << ")" << endl;
             delete current; // delete the node
         }
         Sleep(100);
@@ -80,19 +99,27 @@ public:
         Sleep(100);
         this->wait = 0;
     }
-    void changename(SOCKET ClientSocketchangename, char chngenome[15]) {
+    int changename(SOCKET ClientSocketchangename, char chngenome[15], int num = 1) {
         Node* thisNode = head;
         while (thisNode != NULL) {
             if (ClientSocketchangename == thisNode->ClientSocket) {
                 strcpy_s(thisNode->name, chngenome);
-                char message[] = "Login Successful";
-                if (!sendd(ClientSocketchangename, message, thisNode->name))return;
-                //cout << "hai";
-                return;
+                char message[20];
+                if (num == 1) {
+                    strcpy_s(message, "#Login Successful");
+                }
+                else if (num == 2) {
+                    strcpy_s(message, "#Account deleted");
+                }
+                else {
+                    strcpy_s(message, "#Logged out");
+                }
+                if (!sendd(ClientSocketchangename, message, thisNode->name))return 0;
+                break;
             }
             thisNode = thisNode->next;
         }
-
+        return num;
     }
     void getonline(SOCKET ClientSocketgetonline, char SendName[15]) {
         Node* thisNode = head;
@@ -118,6 +145,20 @@ public:
 
         }
         if (!sendd(ClientSocketgetonline, name, SendName))return;
+    }
+    int sendonepersonfromserver(char servermessage[DEFAULT_BUFLEN], char fromname[15], char toname[15]) {
+        Node* thisNode = head;
+        while (thisNode != NULL) {
+            if (strcmp(thisNode->name, fromname) == 0) {
+                char newmessage[DEFAULT_BUFLEN] = "\0";
+                sprintf_s(newmessage, "%s(seen by %s)(server)", servermessage, toname);
+                cout << newmessage << endl;
+                if (!sendd(thisNode->ClientSocket, newmessage, fromname))return 0;
+                return 1;
+            }
+            thisNode = thisNode->next;
+        }
+        return 0;
     }
     int sendoneperson(SOCKET ClientSocketsendoneperson, char ReceiveName[15], char sendmessage[DEFAULT_BUFLEN], char SendName[15]) {
         Node* thisNode = head;
